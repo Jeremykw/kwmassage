40253e4cc3ff44ac905a8f14c1adc2cc91e6f04c
Jeremy Bissonnette <jeremy@kwmassage.com>
2015-12-03 15:53:32 -0500

    remove node_modules/

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,27 @@
 var gulp = require('gulp'),
-	uglify = require('gulp-uglify');
+	uglify = require('gulp-uglify'),
+	minifyCSS = require('gulp-minify-css'),
+	minifyHTML = require('gulp-minify-html');
 
-gulp.task('minifyJs', function() {
-	gulp.src('js/*.js')
+const SITE = '../kwmassage-stage' /* '_site_production'
+
+/* Minify JS */
+gulp.task('js', function() {
+	gulp.src(SITE + '/js/*.js')
 	.pipe(uglify())
-	.pipe(gulp.dest('_site_production/js'))
+	.pipe(gulp.dest(SITE + '/js'))
+});
+
+/* Minify CSS */
+gulp.task('css', function() {
+	gulp.src(SITE + '/**/*.css' )
+	.pipe(minifyCSS())
+	.pipe(gulp.dest(SITE))
+});
+
+/* Minify HTML */
+gulp.task('min', ['css', 'js'], function() {
+	gulp.src(SITE + '/**/**/*.html')
+	.pipe(minifyHTML())
+	.pipe(gulp.dest(SITE))
 });
